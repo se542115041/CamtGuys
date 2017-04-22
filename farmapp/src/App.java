a475938f5b823a35877c1cddcce85cfcbb602956
se542115041 <se542115041@vr.camt.info>
2017-04-22 21:12:27 +0700

    Added main class name "App" to run all features.

---
+++ farmapp/src/App.java
@@ -0,0 +1,11 @@
+import CamtGuys.farmapp.view.prompt;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.print("Welcome to the World of Garden! ");
+	      prompt run = new prompt();
+	      run.userMenu();
+	}
+
+}
