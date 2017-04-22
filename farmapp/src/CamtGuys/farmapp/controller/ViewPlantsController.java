39c3378528eeb5235551df836b2f4dc4ebe31815
se542115041 <se542115041@vr.camt.info>
2017-04-22 21:14:18 +0700

    Added view plants feature but not completed yet. GH-01

---
+++ farmapp/src/CamtGuys/farmapp/controller/ViewPlantsController.java
@@ -0,0 +1,16 @@
+package CamtGuys.farmapp.controller;
+
+
+public class ViewPlantsController  {
+	
+	public void ViewPlants(){
+		
+		PlantController plantController = new PlantController();
+		System.out.println(plantController.getPlant().size());
+		System.out.println("not yet");
+		
+		
+		
+	}
+
+}
