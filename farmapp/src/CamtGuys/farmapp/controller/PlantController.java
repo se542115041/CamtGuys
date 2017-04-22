a2d17c4d297f86123d56791ac5ad97dcd9c6f4a8
se542115041 <se542115041@vr.camt.info>
2017-04-22 21:11:48 +0700

    Add Plant feature to user options.

--- farmapp/src/CamtGuys/farmapp/controller/PlantController.java
+++ farmapp/src/CamtGuys/farmapp/controller/PlantController.java
@@ -1,5 +1,87 @@
 package CamtGuys.farmapp.controller;
 
-public class PlantController {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+import CamtGuys.farmapp.model.BigPlant;
+import CamtGuys.farmapp.view.prompt;
+
+
+public class PlantController{
+	ArrayList<BigPlant>	bp = new ArrayList<BigPlant>();
+	prompt run = new prompt();
+	Scanner selectSeed = new Scanner(System.in);
+	BigPlant bigPlant = new BigPlant();
+	int selectedSeed;
+
+	public ArrayList<BigPlant> plant(){   
+		
+		
+		System.out.println("Select a seed to plant\n"
+				+ "1: Papaya\n"
+				+ "2: Coconut\n"
+				+ "3: Watermellon\n");
+		selectedSeed= selectSeed.nextInt();
+			if(selectedSeed == 1){
+        		
+        			bigPlant.setName("Papayafruit");
+        			bigPlant.setAge(1);
+        			bigPlant.setAgeofFruitable(14);
+        			bigPlant.setHP(4.0);
+        			bigPlant.setNumbOfFruit(0);
+        			
+        			bp.add(bigPlant);
+        		
+        			System.out.println(bp.get(0).getName()+" Seed Planted.");
+        			System.out.println(bp.get(0).getHP());
+        			System.out.println(bp.get(0).getNumbOfFruit());
+        			System.out.println(bp.get(0).getAgeofFruitable());
+        			//run.userMenu();
+        			//selectSeed.close();
+        	}
+		
+			else if(selectedSeed == 2){
+    		
+			bigPlant.setName("Coconut");
+			bigPlant.setAge(1);
+			bigPlant.setAgeofFruitable(14);
+			bigPlant.setHP(4.0);
+			bigPlant.setNumbOfFruit(0);
+			
+			bp.add(bigPlant);
+		
+			System.out.println(bigPlant.getFruit()+" Seed Planted.");
+			//selectSeed.close();
+			//run.userMenu();
+			
+			
+			}
+		
+			else if(selectedSeed == 3){
+    		
+			bigPlant.setName("Watermellon");
+			bigPlant.setAge(1);
+			bigPlant.setAgeofFruitable(14);
+			bigPlant.setHP(4.0);
+			bigPlant.setNumbOfFruit(0);
+			
+			bp.add(bigPlant);
+		
+		
+			System.out.println(bp.get(0).getName()+" Seed Planted.");
+			//selectSeed.close();
+			//run.userMenu();
+			
+			
+			}
+		else {
+			System.out.println("Wrong choice!");
+			
+		}run.userMenu();
+			return bp;		
+	}	
+	public ArrayList<BigPlant> getPlant(){
+		return this.bp;
+		
+	}
 }
