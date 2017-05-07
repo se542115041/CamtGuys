b967d2b3465922beec1cabf68f6c616fae7e7313
tanaphum <tana_jumbo@hotmail.com>
2017-05-07 15:26:55 +0700

    Update Menu
    Update Menu

--- farmapp/src/CamtGuys/farmapp/view/prompt.java
+++ farmapp/src/CamtGuys/farmapp/view/prompt.java
@@ -1,27 +1,34 @@
 package CamtGuys.farmapp.view;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import CamtGuys.farmapp.controller.Market;
 import CamtGuys.farmapp.controller.PlantController;
 import CamtGuys.farmapp.controller.ViewPlantsController;
 import CamtGuys.farmapp.model.BigPlant;
+import CamtGuys.farmapp.model.Plant;
 
 public class prompt {
 	int menu;
 	Scanner selectMenu = new Scanner(System.in);
-	
+	List<Plant> seedplant = new ArrayList<Plant>();
 	ViewPlantsController view = new ViewPlantsController();
 	public void userMenu(){
-
+		do{
 		System.out.println("Welcome to the World of Garden! Please choose an action\n"
 				+ "(1) View Plants\n"
 				+ "(2) Plant a plant\n"
-				+ "(3) Sleep\n");
+				+ "(3) Sleep");
+		System.out.println("(4) go to market");
+		System.out.println("(0) Exit");
+		System.out.println();
+		System.out.print("selectMenu : ");
 		
 		menu = selectMenu.nextInt();
 		
 	   if(menu == 1){
-		   selectMenu.close();
+
 	        		view.ViewPlants();
 	        		
 	        		
@@ -33,17 +40,29 @@ public class prompt {
 	        		// selectMenu.close();
 	   }
 	   else if(menu==3){
-		   selectMenu.close();
 		   System.out.println("coming soon!");
 		   
 		   userMenu();
 		   
 	    }
-	   else{
-		   selectMenu.close();
-		   System.out.println("you're drunk!");
-		   selectMenu.close();
+	   else if(menu==4){
+		   System.out.println("go to market");
+		   Market market = new Market();
+		   market.Show();
+		   userMenu();
+		   
+	    }
+	   else if(menu==0){
+		   System.out.println("bye bye!");
+
 	   }
+	   else{
+		   System.out.println("you're drunk!");
+
+	   }
+	   selectMenu.reset();
+	}while(menu != 0);
+		   selectMenu.close();
 	}
 }
 
